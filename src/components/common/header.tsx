1cfc98015ddf8df91ab14fbc0dc00be3e1fffc57
sisu <danielcoder1022@gmail.com>
2024-12-15 09:40:20 +0900

    fix some

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,7 +1,8 @@
 const Header = () => {
     return (
         <div className="bg-[#3f4652] h-12 border-b-[1px] border-gray-400 flex w-full items-center justify-between p-2">
-            <h1 className="font-bold text-white">BREMEN</h1>
+            {/* <h1 className="font-bold text-white">BREMEN</h1> */}
+            <img src="./companylogo.png" alt="CompanyLogo" className="w-28 h-8" />
         </div>
     )
 }
--- src/components/common/leftbar.tsx
+++ src/components/common/leftbar.tsx
@@ -25,11 +25,11 @@ const LeftBar = ({ productnumber, setProductNumber, barname, setBarName }: Numbe
             <div className='h-28 border-b border-gray-400 p-3'>
                 <div className='flex justify-between mb-2'>
                     <div className='flex flex-col'>
-                        <span className='text-white font-semibold'>{maskIndex == 0 ? 'レギュラーフラグ' : 'スイングフラッグ'}</span>
+                        <span className='text-white font-semibold'>{maskIndex == 0 ? 'レギュラーフラッグ' : 'スイングフラッグ'}</span>
                         <span className='text-white text-xs'>90cm x 136cm</span>
                     </div>
                     <div className='flex items-center px-2 bg-[#FF0000] rounded-md h-8'>
-                        <span className='text-white text-base'>¥ {maskIndex == 0 ? '5,390' : '9,790'}/{1}枚</span>
+                        <span className='text-white text-base'>¥{maskIndex == 0 ? '5,390 ' : '9,790 '}/{' 1'}枚</span>
                     </div>
                 </div>
                 <NumberCount productnumber={productnumber} setProductNumber={setProductNumber} />
