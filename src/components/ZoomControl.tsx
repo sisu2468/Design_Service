8b09e10ccb5eb24001ebc047fbbc5a0a427e43e7
to-journey <bs9961030@gmail.com>
2024-12-17 08:58:34 +0900

    feat: update-zoom-scale

--- src/components/ZoomControl.tsx
+++ src/components/ZoomControl.tsx
@@ -7,11 +7,11 @@ const ZoomControl = () => {
 
     return (
         <div className="absolute right-6 bottom-6 bg-[#3F4652] flex">
-            <div className="w-[32px] h-[32px] flex justify-center items-center" onClick={() => setZoom(Math.max(0.25, zoom - 0.1))}>
+            <div className="w-[32px] h-[32px] flex justify-center items-center" onClick={() => setZoom(Math.max(0.10, zoom - 0.1))}>
                 <FaMinus color="white" />
             </div>
             <div className="px-4 flex items-center border-l border-r border-[#616875]">
-                <input type="range" min="25" max="250" value={zoom * 100} onChange={(e) => setZoom(parseInt(e.target.value) / 100)} />
+                <input type="range" min="10" max="250" value={zoom * 100} onChange={(e) => setZoom(parseInt(e.target.value) / 100)} />
                 <p className="w-[40px] text-white text-right">
                     {(zoom * 100).toFixed(0)}%
                 </p>
