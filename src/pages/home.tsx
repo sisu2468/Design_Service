3c6abea9603755f81d9136dce9325ca912f29f8f
to-journey <bs9961030@gmail.com>
2024-12-16 13:58:48 +0900

    fix: show-image-edit-buttons

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Canvas from "../components/Canvas";
 import BodyHeader from "../components/common/body-header";
 import Header from "../components/common/header";
@@ -7,24 +7,15 @@ import ZoomControl from "../components/ZoomControl";
 
 export default function Home() {
     const [productnumber, setProductNumber] = useState<number>(1);
-    const [barstatus, setBarStatus] = useState(false);
     const [barname, setBarName] = useState('items');
 
-    useEffect(() => {
-        if (barname == 'images' || barname === 'freeimages') {
-            setBarStatus(true);
-        }
-        else {
-            setBarStatus(false);
-        }
-    }, [barname])
     return (
         <div className="h-screen flex flex-col">
             <Header />
             <div className="flex-[1_1_0] flex">
                 <LeftBar productnumber={productnumber} setProductNumber={setProductNumber} barname={barname} setBarName={setBarName} />
                 <div className="relative w-[calc(100%-320px)] flex flex-col">
-                    <BodyHeader productnumber={productnumber} barstatus={barstatus} setProductNumber={setProductNumber} />
+                    <BodyHeader productnumber={productnumber} setProductNumber={setProductNumber} />
                     <div className="relative flex-[1_1_0] bg-white overflow-auto">
                         <Canvas />
                     </div>
